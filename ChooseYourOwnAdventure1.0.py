38acad56b68154213aefe8c522584edf003e9367
Williams <23678bella23678@gmail.com>
2024-03-18 13:26:08 -0400

    Introductions prints

--- ChooseYourOwnAdventure1.0.py
+++ ChooseYourOwnAdventure1.0.py
@@ -1,3 +1,10 @@
 # Programmer: Bella Williams
 # Date: 3.15.2024
 # Program: Choose Your Own Adventure
+
+print("**********************************************************")
+print("\nWelcome to Boyfriend Simulator!\n")
+print("In this simulation you will be given scenarios and options")
+print("that will determine how your relationship works out!")
+print("\n\nThis is your opportunity to see what encounters you might come")
+print("across in a relationship and play around with it.")
